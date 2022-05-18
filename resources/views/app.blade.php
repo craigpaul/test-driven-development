2e93df1c6fe43cf286afdde2f6d17ffa9f980f49
Craig <craig.paul@coconutsoftware.com>
2022-05-17 22:00:16 -0600

    Sets up skeleton React application

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="h-full min-h-full" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <title>To-Dos</title>
@@ -8,8 +8,8 @@
         <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     </head>
-    <body>
-        <div id="root"></div>
+    <body class="h-full min-h-full">
+        <div class="h-full min-h-full bg-gray-100" id="root"></div>
         <script src="{{ mix('/js/app.js') }}"></script>
     </body>
 </html>
