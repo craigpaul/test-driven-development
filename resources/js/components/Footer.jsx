be955ee966c8401b18f660a82dd2974c60a5a28d
Craig <craig.paul@coconutsoftware.com>
2022-05-18 06:45:11 -0600

    Applies code styling fixes

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -4,13 +4,12 @@ function Footer() {
   const { items } = useToDos();
 
   const uncompletedItems = items.filter((item) => item.completed);
+  const length = uncompletedItems.length;
+  const message = `${length} ${length === 1 ? 'item' : 'items'} left`;
 
   return (
     <footer className="w-full border-t border-gray-200 py-2 px-4 text-sm font-light text-gray-300">
-      <p>
-        {uncompletedItems.length}{' '}
-        {uncompletedItems.length === 1 ? 'item' : 'items'} left
-      </p>
+      <p>{message}</p>
     </footer>
   );
 }
