6141421ca951c3b61525b753c6d465fec8737c37
Craig <craig.paul@coconutsoftware.com>
2022-05-18 06:26:10 -0600

    Outlines a basic set of tests to implement for performing CRUD operations with ToDos

---
+++ tests/Feature/CanPerformVariousActionsWithToDosTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class CanPerformVariousActionsWithToDosTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testCanCreateNewToDo()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    public function testCanReadExistingToDos()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    public function testCanUpdateExistingToDo()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    public function testCanDeleteExistingToDo()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
