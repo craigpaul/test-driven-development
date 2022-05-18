b8cedbaffc3fb008ce14518c19bf27f79ddcb2b3
Craig <craig.paul@coconutsoftware.com>
2022-05-18 10:27:41 -0600

    Marks decorative element as hidden for accessibility reasons

--- resources/js/components/Form.jsx
+++ resources/js/components/Form.jsx
@@ -6,7 +6,7 @@ function Form() {
   return (
     <header className="relative h-16 w-full py-2 pl-14 pr-4">
       {items.length > 0 ? (
-        <div className="absolute left-4 top-1/2 -translate-y-1/2 transform text-gray-300">
+        <div aria-hidden="true" className="absolute left-4 top-1/2 -translate-y-1/2 transform text-gray-300">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
