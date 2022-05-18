c10fcc13cd7637c3a57b90ac631b9a3e5644abea
Craig <craig.paul@coconutsoftware.com>
2022-05-17 21:41:20 -0600

    Adds route definition and skeleton HTML for loading the application

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>To-Dos</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+        <link rel="icon" href="/favicon.ico" />
+    </head>
+    <body>
+        <div id="root"></div>
+    </body>
+</html>
