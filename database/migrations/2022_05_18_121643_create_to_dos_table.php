d5c6411ffd7ea5bad47228a2d326e4d321ff851f
Craig <craig.paul@coconutsoftware.com>
2022-05-18 06:20:54 -0600

    Adds ToDo model, migration and factory

---
+++ database/migrations/2022_05_18_121643_create_to_dos_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('to_dos')) {
+            return;
+        }
+
+        Schema::create('to_dos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->boolean('completed');
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+};
