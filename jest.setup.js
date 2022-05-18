d68b3b3163708300363172bdd1fd298c38736f6e
Craig <craig.paul@coconutsoftware.com>
2022-05-18 09:31:11 -0600

    Sets up a mock server for testing purposes

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom'
+import { server } from './resources/js/mocks/server'
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
